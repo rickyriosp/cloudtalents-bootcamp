699903e05a99bfcf0ae2fc5b619358cd41ade9ed
rickyriosp <riki.rios91@gmail.com>
2025-04-04 21:59:18 -0400

    cdk

---
+++ cdk/test/cloudtalents-bootcamp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudtalentsBootcamp from '../lib/cloudtalents-bootcamp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloudtalents-bootcamp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudtalentsBootcamp.CloudtalentsBootcampStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
